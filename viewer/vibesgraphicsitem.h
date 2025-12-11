19371fc636bc2c0ef97da502f3274f7fec75956f
godardma <mael.godard@ensta-bretagne.org>
2025-12-11 17:19:35 +0100

    adding ZValue

--- viewer/vibesgraphicsitem.h
+++ viewer/vibesgraphicsitem.h
@@ -115,6 +115,7 @@ class VibesGraphicsItem
     QGraphicsItem * _qGraphicsItem;
     QString _name;
     int _dimX, _dimY;
+    double _zValue; // if item is in a group, the distinction is needed between global ZValue and "group" ZValue
 public:
     enum { VibesGraphicsItemType = QGraphicsItem::UserType,
            // Group of item
@@ -165,6 +166,11 @@ public:
     QString name() const { return _name; }
     void setName(QString name) { if (name != this->name()) { _name=name; if (scene()) scene()->setItemName(this, this->name()); } }
     VibesScene2D* scene() const { if (_qGraphicsItem) return static_cast<VibesScene2D*>( _qGraphicsItem->scene() ); else return 0;}
+
+    void setInitialZValue(double z) { _zValue = z; }
+    double initialZValue() const { return _zValue; }
+    void setZValue(qreal z) { if (_qGraphicsItem) _qGraphicsItem->setZValue(z); }
+    double zValue() const { if (_qGraphicsItem) return _qGraphicsItem->zValue(); else return 0.; }
 
     operator QGraphicsItem& () { Q_ASSERT(_qGraphicsItem!=0); return *_qGraphicsItem; }
     operator const QGraphicsItem& () const { Q_ASSERT(_qGraphicsItem!=0); return *_qGraphicsItem; }
